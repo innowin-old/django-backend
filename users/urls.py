39f47e064b0434a23e047b48fde9a117f3a8ce78
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2017-02-27 14:17:43 +0330

    fix email activation url

--- users/urls.py
+++ users/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
             'template_name': 'password_reset_confirm.html',
             'post_reset_redirect': 'login'},
         name='password_reset_confirm'),
-    url(r'^active/(?P<token>[0-9A-Za-z:-]+)/$', views.active_user, name='active'),
+    url(r'^active/(?P<token>[0-9A-Za-z:_\-]+)/$', views.active_user, name='active'),
 ]
