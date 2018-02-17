5b6776c5c56fe64089973c207657d1845d85f660
Amir Kamankesh <kamankesh.amr@gmail.com>
2018-02-17 20:37:32 +0330

    Close #46

--- exchanges/permissions.py
+++ exchanges/permissions.py
@@ -2,6 +2,8 @@ from rest_framework import permissions
 
 from .models import ExchangeIdentity
 
+from django.conf import settings
+
 
 class IsExchangeOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -26,4 +28,15 @@ class IsExchangeIdentity(permissions.BasePermission):
                                                                 )
             if get_user_exchange.count() != 0 or request.user.is_superuser:
                 return True
-        return False
+        return True
+
+
+class IsExchangeFull(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == "POST":
+            exchange_id = request.POST['exchange_identity_related_exchange']
+            if exchange_id is not None:
+                exchange_count = ExchangeIdentity.objects.filter(exchange_identity_related_exchange_id=exchange_id).count()
+                if exchange_count > settings.EXCHANGE_LIMIT:
+                    return False
+        return True
