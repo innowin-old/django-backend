81b1f829d98b1de838ac883359570660abdfef87
Amir Kamankesh <kamankesh.amr@gmail.com>
2018-03-08 17:33:03 +0330

    #33

--- users/views.py
+++ users/views.py
@@ -49,7 +49,7 @@ from .permissions import IsIdentityOwnerOrReadOnly, IsSuperUserOrReadOnly, IsUrl
 
 
 class UserViewset(ModelViewSet):
-    permission_classes = [AllowAny]
+    permission_classes = [IsSuperUserOrReadOnly, IsAuthenticated]
 
     def get_queryset(self):
         if self.request.user.is_superuser:
@@ -166,7 +166,7 @@ class IdentityViewset(ModelViewSet):
 
 class ProfileViewset(ModelViewSet):
     owner_field = 'profile_user'
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated, BlockPostMethod, IsOwnerOrReadOnly]
 
     def get_queryset(self):
         queryset = Profile.objects.all()
