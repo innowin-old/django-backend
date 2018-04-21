0fd16e47b6a807f643bbe4693da69c6aa71a3b12
Amir Kamankesh <kamankesh.amr@gmail.com>
2018-04-21 17:03:27 +0430

    Close #66

--- displacements/urls.py
+++ displacements/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
-from .views import GetUserDataViewset, GetProductDataViewset, GetOrganizationDataViewSet
+from .views import GetUserDataViewset, GetProductDataViewset, GetOrganizationDataViewSet, GetExchangeDataViwSet
 
 router = DefaultRouter()
 
+router.register(r'exchanges-data', GetExchangeDataViwSet, 'exchanges-data')
 router.register(r'organizations-data', GetOrganizationDataViewSet, 'organizations-data')
 router.register(r'products-data', GetProductDataViewset, 'products-data')
 router.register(r'users-data', GetUserDataViewset, 'users-data')
--- displacements/views.py
+++ displacements/views.py
@@ -2,9 +2,10 @@ from django.contrib.auth.models import User
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
 
+from exchanges.models import Exchange
 from products.models import Product
 from organizations.models import Organization
-from .serializers import GetUserDataSerializer, GetProductDataSerializer, GetOrganizationDataSerializer
+from .serializers import GetUserDataSerializer, GetProductDataSerializer, GetOrganizationDataSerializer, GetExchangeDataSerializer
 
 
 # Create your views here.
@@ -39,3 +40,14 @@ class GetOrganizationDataViewSet(ModelViewSet):
 
     def get_serializer_class(self):
         return GetOrganizationDataSerializer
+
+
+class GetExchangeDataViwSet(ModelViewSet):
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        queryset = Exchange.objects.all()
+        return queryset
+
+    def get_serializer_class(self):
+        return GetExchangeDataSerializer
