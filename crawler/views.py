6153789e79f3fe278b4d98e6a204199e70c3dc8b
Amir Kamankesh <kamankesh.amr@gmail.com>
2018-04-26 20:44:42 +0430

    Close #73

--- crawler/views.py
+++ crawler/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-from exchanges.models import Exchange
+from bs4 import BeautifulSoup
+import requests
+import string
+from django.db import transaction
+
+from crawler.models import Topic
 
 
 # Create your views here.
 def crawl_research_gate():
-    exchanges = Exchange.objects.all()
-    print(exchanges)
-    print('salam')
+    with transaction.atomic():
+        topic_entities = []
+        characters = list(string.ascii_uppercase)
+        for character in characters:
+            print('In page with character {0}'.format(character))
+            response = requests.get('https://www.researchgate.net/topics/{0}'.format(character))
+            soup = BeautifulSoup(response.content, 'html.parser')
+            topics = soup.find_all(name='a', attrs={'class': 'js-score-goal'})
+            for topic in topics:
+                topic_entity = Topic(name=topic.get_text())
+                topic_entities.append(topic_entity)
+        print('Adding topics to database :))))))))))')
+        Topic.objects.bulk_create(topic_entities)
+        print('OK !')
