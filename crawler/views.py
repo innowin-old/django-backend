ad84934b2d1bba736dde6e34c42427d84c08549f
Amir Kamankesh <kamankesh.amr@gmail.com>
2018-04-26 18:24:35 +0430

    ru views in shell tested

--- crawler/views.py
+++ crawler/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from exchanges.models import Exchange
+
 
 # Create your views here.
+def crawl_research_gate():
+    exchanges = Exchange.objects.all()
+    print(exchanges)
+    print('salam')
