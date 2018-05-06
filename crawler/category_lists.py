89ef865eb91c42fd588a61f1600f1b8f72d38a5c
Amir Kamankesh <kamankesh.amr@gmail.com>
2018-05-06 17:47:38 +0430

    Close #76

--- crawler/category_lists.py
+++ crawler/category_lists.py
@@ -4,5 +4,6 @@ VITRINNET_CATEGORIES = [
     'farming-parts', 'textile-industry-machinery', 'textile-industry-equipment', 'textile-industry-parts', 'Construction-industry-machinery',
     'Construction-industry-equipment', 'Heating-and-cooling-installations', 'wood-industry-machinery', 'wood-industry-equipment',
     'wood-industry-parts', 'Cellulose-and-paper-machine', 'Cellulose-and-paper-equipment', 'Cellulose-and-paper-parts',
-
+    'chemichal', 'color-industry', 'printing-industry', 'medical', 'electricity', 'Metal-Processing-Industries', 'Livestock-and-poultry-industries',
+    'other-industrial-machinery-and-equipments', 'car', 'Industrial-cleaning', 'oil-and-gas-industry'
 ]
