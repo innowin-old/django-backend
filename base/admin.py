e7ca7a54c65e00ebce396fc5925d34334eb33f13
Saeid Ramezani <mail.saeid.ramezani@gmail.com>
2018-12-06 00:08:14 +0330

    Add coutnry, province and town in admin

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import RollPermission, Post
+from .models import RollPermission, Post, BaseCountry, BaseProvince, BaseTown
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['pk', 'post_title', 'post_identity', 'post_type', 'post_user', 'post_parent']
@@ -7,3 +7,6 @@ class PostAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(RollPermission)
 admin.site.register(Post, PostAdmin)
+admin.site.register(BaseCountry)
+admin.site.register(BaseProvince)
+admin.site.register(BaseTown)
