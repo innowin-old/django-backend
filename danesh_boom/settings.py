aadd6da08ac0f376d12da6b56fdd79406617049b
Saeid Ramezani <mail.saeid.ramezani@gmail.com>
2018-12-11 15:50:49 +0330

    Latest changes

--- danesh_boom/settings.py
+++ danesh_boom/settings.py
@@ -89,7 +89,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 20,
     'DEFAULT_THROTTLE_CLASSES': (
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle'
