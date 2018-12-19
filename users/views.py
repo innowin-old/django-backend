6cd4d21b3f12ae661340ef9eadee22b8cfe0b5a6
Saeid Ramezani <mail.saeid.ramezani@gmail.com>
2018-12-19 15:24:56 +0330

    Send email test

--- users/views.py
+++ users/views.py
@@ -1151,7 +1151,8 @@ class ForgetPasswordViewset(ViewSet):
                 message = settings.EMAIL_TEXT + '\n' + ' کد : ' + str(user_code.code)
                 email_from = settings.EMAIL_HOST_USER
                 recipient_list = [user.email, ]
-                send_mail(subject, message, email_from, recipient_list)
+                # send_mail(subject, message, email_from, recipient_list)
+                send_mail('Subject', 'Test', 'saeidrd90@gmail.com', ['saeid@varpex.com'], fail_silently=False)
             return Response({'detail': 'code sended'}, status=status.HTTP_200_OK)
         else:
             return Response({'detail': 'invalid data'}, status=status.HTTP_400_BAD_REQUEST)
